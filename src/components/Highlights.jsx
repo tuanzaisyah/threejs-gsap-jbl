14f85eb1e9e96eab96ac48f7213782cd67d56108
tuanzaisyah <tuannurulaisyah@gmail.com>
2024-08-25 00:09:25 +0800

    feat: add animation for columns at highlight section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,10 +1,90 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import Highlight1 from "../assets/highlight1.png";
 import Highlight2 from "../assets/highlight2.png";
 import Highlight3 from "../assets/highlight3.png";
 import Highlight4 from "../assets/highlight4.png";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Highlights = () => {
+  const column1 = useRef();
+  const column2 = useRef();
+  const column3 = useRef();
+  const column4 = useRef();
+
+  useGSAP(() => {
+    gsap.fromTo(
+      column1.current,
+      { x: -50, opacity: 0 },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: column1.current,
+          start: "top 80%",
+          end: "bottom 20%",
+          scrub: 1,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      column2.current,
+      { x: 50, opacity: 0 },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: column2.current,
+          start: "top 80%",
+          end: "bottom 20%",
+          scrub: 1,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      column3.current,
+      { x: -50, opacity: 0 },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: column3.current,
+          start: "top 80%",
+          end: "bottom 50%",
+          scrub: 1,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      column4.current,
+      { x: 50, opacity: 0 },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: column4.current,
+          start: "top 80%",
+          end: "bottom 50%",
+          scrub: 1,
+        },
+      }
+    );
+  }, []);
+
   return (
     <section
       id="highlights"
@@ -12,7 +92,10 @@ const Highlights = () => {
     >
       <div className="screen-max-width px-6 py-10 flex flex-col gap-6 md:gap-4">
         <div className="flex flex-col md:flex-row gap-6 md:gap-4">
-          <div className="glassmorphism rounded-2xl md:basis-2/3 flex flex-col md:flex-row">
+          <div
+            ref={column1}
+            className="glassmorphism rounded-2xl md:basis-2/3 flex flex-col md:flex-row "
+          >
             <img
               src={Highlight1}
               alt="highlight1"
@@ -28,7 +111,7 @@ const Highlights = () => {
               </p>
             </div>
           </div>
-          <div className="glassmorphism rounded-2xl md:basis-1/3">
+          <div ref={column2} className="glassmorphism rounded-2xl md:basis-1/3">
             <img
               src={Highlight2}
               alt="highlight2"
@@ -46,7 +129,7 @@ const Highlights = () => {
           </div>
         </div>
         <div className="flex flex-col md:flex-row gap-6 md:gap-4">
-          <div className="glassmorphism rounded-2xl md:basis-2/5">
+          <div ref={column3} className="glassmorphism rounded-2xl md:basis-2/5">
             <img
               src={Highlight3}
               alt="highlight3"
@@ -62,7 +145,10 @@ const Highlights = () => {
               </p>
             </div>
           </div>
-          <div className="flex flex-col gap-6 md:gap-4 md:basis-3/5">
+          <div
+            ref={column4}
+            className="flex flex-col gap-6 md:gap-4 md:basis-3/5"
+          >
             <div className="glassmorphism rounded-2xl flex flex-col md:flex-row basis-3/4">
               <img
                 src={Highlight4}
